3afecf4f1f5db46e48219b9808be88da3e662098
tebuu24 <tereze.bogdane27@gmail.com>
2024-04-22 09:41:18 +0300

    js file cleaner

--- 11e_Server/fun.js
+++ 11e_Server/fun.js
@@ -1,25 +1,3 @@
-function deleteUser(userId) {
-    // Confirm deletion
-    if (confirm('Vai jūs vēlaties dzēst šo lietotāju')) {
-        // Send DELETE request to Flask server
-        fetch(`/delete_user/${userId}`, {
-            method: 'DELETE'
-        })
-        .then(response => {
-            if (response.ok) {
-                // Remove the row from the HTML table
-                document.getElementById(`user_row_${userId}`).remove();
-            } else {
-                // Display error message if deletion fails
-                alert('Kļūda dzēšot lietotāju');
-            }
-        })
-        .catch(error => {
-            // Handle network or server errors
-            console.error('Error:', error);
-        });
-    }
-}
 // Update the form submission event listener
 document.getElementById('myForm').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent default form submission
